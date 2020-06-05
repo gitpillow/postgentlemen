49ac46264991b74b5bb2ca3b5c51cba8b61dd42c
王敏俊10253896 <wang.minjun@zte.com.cn>
2020-06-05 16:27:45 +0800

    remove common.go

--- pgmenui/layout.go
+++ pgmenui/layout.go
@@ -2,6 +2,7 @@ package pgmenui
 
 import (
 	"github.com/jroimartin/gocui"
+	"log"
 )
 
 func Layout(g *gocui.Gui) {
@@ -10,4 +11,20 @@ func Layout(g *gocui.Gui) {
 	listWidget := NewListWidget(0, 0, maxX/3, maxY-1)
 	g.SetManager(listWidget)
 	g.SetCurrentView(listWidget.name)
+}
+
+func Quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
+}
+
+func Load(g *gocui.Gui) {
+	Layout(g)
+
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, Quit); err != nil {
+		log.Panicln(err)
+	}
+
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Panicln(err)
+	}
 }
